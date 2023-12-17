cacc7fdbf38a9142d647af7b3a48f92ecf2f8b4c
Aliaksei Harabchuk <aliaksei.harabchuk@gmail.com>
2023-12-17 07:40:46 +0100

    fix settings

--- docker_deploy/wsgi.py
+++ docker_deploy/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'docker_deploy.landing')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'docker_deploy.settings')
 
 application = get_wsgi_application()
